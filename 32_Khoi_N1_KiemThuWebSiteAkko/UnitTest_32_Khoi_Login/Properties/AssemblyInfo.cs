2a6726663464056262f213c26f1c75969b7114fe
duykhoi1504 <voduykhoi1504@gmail.com>
2024-04-29 19:12:36 +0700

    change to akko and finish login testing

---
+++ 32_Khoi_N1_KiemThuWebSiteAkko/UnitTest_32_Khoi_Login/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_32_Khoi_Login")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_32_Khoi_Login")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91613ab5-303c-48ae-8424-58884872927f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 32_Khoi_N1_KiemThuWebSiteAkko/UnitTest_32_Khoi_Login/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+namespace UnitTest_32_Khoi_Login
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+
+
+
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+                     @".\data\LoginData.csv", "LoginData#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void Test_Login_32_Khoi()
+        {
+
+            /*
+            Test case 1_Đăng nhập thành công
+            Test case 2_Đăng nhập sai tài khoản
+            Test case 3_Đăng nhập sai mật khẩu
+            Test case 4_Đăng nhập thiếu tài khoản
+            Test case 5_Đăng nhập thiếu mật khẩu
+            Test case 6_Đăng nhập với tài khoản có kí tự đặc biệt
+            Test case 7_Đăng nhập với tài khoản được tạo nhưng chưa kích hoạt
+            */
+
+            //lưu dữ liệu ra biến
+            var username = TestContext.DataRow[0].ToString();
+            var password = TestContext.DataRow[1].ToString();
+
+            ChromeDriverService chrome_khoi_32 = ChromeDriverService.CreateDefaultService();
+            chrome_khoi_32.HideCommandPromptWindow = true;
+            IWebDriver driver = new ChromeDriver(chrome_khoi_32);
+
+            // Gọi trang web
+            driver.Navigate().GoToUrl("https://en.akkogear.com/");
+            Thread.Sleep(2000);
+
+
+            driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/div[2]/div/div[1]/div[2]/a")).Click();
+            driver.FindElement(By.Name("user_name")).SendKeys(username);
+            driver.FindElement(By.CssSelector("input[type='password']")).SendKeys(password);
+            driver.FindElement(By.ClassName("login_btn")).Click();
+            Assert.AreEqual("https://en.akkogear.com/my-account/", driver.Url, "Login Fail!");
+         
+     
+
+            //    driver.FindElement(By.ClassName("akko_nav")).Click();
+            //    driver.FindElement(By.ClassName("akko_sub_menu_li")).Click();
+            //}
+        }
+    }
+}
