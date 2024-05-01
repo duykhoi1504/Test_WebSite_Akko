4fdb42ebe33690d622c4d5cc316cd55091fc8202
duykhoi1504 <voduykhoi1504@gmail.com>
2024-05-02 01:20:36 +0700

    update ưord2

--- 32_Khoi_N1_KiemThuWebSiteAkko/32_Khoi_N1_KiemThuWebSiteAkko/Form1.Designer.cs
+++ 32_Khoi_N1_KiemThuWebSiteAkko/32_Khoi_N1_KiemThuWebSiteAkko/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.btn_32_Khoi_TruyCapTrangWeb = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,13 +47,23 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "TestCase_Login";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "test 2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(23, 79);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(244, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button1";
+            this.button2.Text = "test 1";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -66,16 +76,6 @@
             this.btn_32_Khoi_TruyCapTrangWeb.Text = "Truy cập trang web";
             this.btn_32_Khoi_TruyCapTrangWeb.UseVisualStyleBackColor = true;
             this.btn_32_Khoi_TruyCapTrangWeb.Click += new System.EventHandler(this.btn_32_Khoi_TruyCapTrangWeb_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(23, 117);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
